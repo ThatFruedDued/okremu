ec57679dcdd2975f474b37cd86656cf0472134c4
Frued Dued <thatfrueddued@github.com>
2020-04-26 12:38:53 -0400

    Update mobilescr.js

--- mobilescr.js
+++ mobilescr.js
@@ -6,4 +6,21 @@ function setFrameSize() {
   frame.style.width = width + "px";
 }
 
+var up = document.getElementById("upbutton");
+var right = document.getElementById("rightbutton");
+var down = document.getElementById("downbutton");
+var left = document.getElementById("leftbutton");
+
+window.addEventListener("touchstart", e => {
+  e.preventDefault();
+});
+
+window.addEventListener("touchmove", e => {
+  e.preventDefault();
+});
+
+window.addEventListener("touchend", e => {
+  e.preventDefault();
+});
+
 setInterval(setFrameSize, 100);
