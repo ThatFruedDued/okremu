cd7be022f1e4581c650f375eb57ce38cdcbbed4a
Frued Dued <thatfrueddued@github.com>
2020-04-27 18:58:19 -0400

    upload buttons, get ready for touch controls

--- mobilescr.js
+++ mobilescr.js
@@ -1,38 +1,170 @@
+var touches = [];
+var prevElements = [];
+var frame = document.getElementById('frame');
+
 function setFrameSize() {
   var height = visualViewport.height * 0.4;
   var width = visualViewport.width;
-  var frame = document.getElementById('frame');
   frame.style.height = height + "px";
   frame.style.width = width + "px";
 }
 
-var up = document.getElementById("upbutton");
-var right = document.getElementById("rightbutton");
-var down = document.getElementById("downbutton");
-var left = document.getElementById("leftbutton");
-
 window.addEventListener("touchstart", e => {
-  if(e.touches.length > 1) {
-    e.preventDefault();
-  } else {
-    return;
-  }
+  e.preventDefault();
+  e.touches.forEach(tStart);
 }, {passive: false});
 
 window.addEventListener("touchmove", e => {
-  if(e.touches.length > 1) {
-    e.preventDefault();
-  } else {
-    return;
-  }
+  e.preventDefault();
+  e.touches.forEach(tMove);
 }, {passive: false});
 
 window.addEventListener("touchend", e => {
-  if(e.touches.length > 1) {
-    e.preventDefault();
-  } else {
-    return;
-  }
+  e.preventDefault();
+  e.touches.forEach(tEnd);
 }, {passive: false});
 
+function tEnd(touch){
+  var toRemove = [touch.clientX, touch.clientY];
+  var index = touches.indexOf(toRemove);
+  touches.splice(index, 1);
+}
+
+function tMove(touch, tnum){
+  touches[tnum] = [touch.clientX, touch.clientY];
+}
+
+function tStart(touch) {
+  touches.push(touch.clientX, touch.clientY);
+}
+
+function touchHandler(){
+  for(var i = 0; i < touches.length; i++){
+    touchedElem = document.elementFromPoint(touches[i][0], touches[i][1]);
+    try {
+      previous = prevElements[i];
+      if(touchedElem === previous){
+        return;
+      } else {
+        switch(previous.id){
+          case "bup":
+            frame.contentWindow.pressButtonUp(38);
+            break;
+          case "bright":
+            frame.contentWindow.pressButtonUp(39);
+            break;
+          case "bdown":
+            frame.contentWindow.pressButtonUp(40);
+            break;
+          case "bleft":
+            frame.contentWindow.pressButtonUp(37);
+            break;
+          case "bb":
+            frame.contentWindow.pressButtonUp(90);
+            break;
+          case "ba":
+            frame.contentWindow.pressButtonUp(32);
+            break;
+          case "bx":
+            frame.contentWindow.pressButtonUp(88);
+            break;
+          case "by":
+            frame.contentWindow.pressButtonUp(89);
+            break;
+          case "bl":
+            frame.contentWindow.pressButtonUp(76);
+            break;
+          case "br":
+            frame.contentWindow.pressButtonUp(82);
+            break;
+          case "bselect":
+            frame.contentWindow.pressButtonUp(16);
+            break;
+          case "bstart":
+            frame.contentWindow.pressButtonUp(13);
+            break;
+        }
+        switch(touchedElem.id){
+          case "bup":
+            frame.contentWindow.pressButtonDown(38);
+            break;
+          case "bright":
+            frame.contentWindow.pressButtonDown(39);
+            break;
+          case "bdown":
+            frame.contentWindow.pressButtonDown(40);
+            break;
+          case "bleft":
+            frame.contentWindow.pressButtonDown(37);
+            break;
+          case "bb":
+            frame.contentWindow.pressButtonDown(90);
+            break;
+          case "ba":
+            frame.contentWindow.pressButtonDown(32);
+            break;
+          case "bx":
+            frame.contentWindow.pressButtonDown(88);
+            break;
+          case "by":
+            frame.contentWindow.pressButtonDown(89);
+            break;
+          case "bl":
+            frame.contentWindow.pressButtonDown(76);
+            break;
+          case "br":
+            frame.contentWindow.pressButtonDown(82);
+            break;
+          case "bselect":
+            frame.contentWindow.pressButtonDown(16);
+            break;
+          case "bstart":
+            frame.contentWindow.pressButtonDown(13);
+            break;
+        }
+      }
+    } catch(err) {
+      switch(touchedElem.id){
+        case "bup":
+          frame.contentWindow.pressButtonDown(38);
+          break;
+        case "bright":
+          frame.contentWindow.pressButtonDown(39);
+          break;
+        case "bdown":
+          frame.contentWindow.pressButtonDown(40);
+          break;
+        case "bleft":
+          frame.contentWindow.pressButtonDown(37);
+          break;
+        case "bb":
+          frame.contentWindow.pressButtonDown(90);
+          break;
+        case "ba":
+          frame.contentWindow.pressButtonDown(32);
+          break;
+        case "bx":
+          frame.contentWindow.pressButtonDown(88);
+          break;
+        case "by":
+          frame.contentWindow.pressButtonDown(89);
+          break;
+        case "bl":
+          frame.contentWindow.pressButtonDown(76);
+          break;
+        case "br":
+          frame.contentWindow.pressButtonDown(82);
+          break;
+        case "bselect":
+          frame.contentWindow.pressButtonDown(16);
+          break;
+        case "bstart":
+          frame.contentWindow.pressButtonDown(13);
+          break;
+      }
+    }
+  }
+}
+
+setInterval(touchHandler, 0);
 setInterval(setFrameSize, 100);
