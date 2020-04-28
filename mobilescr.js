6a8560968a5bc347532f4f285e8ae146dfd38bca
Frued Dued <thatfrueddued@github.com>
2020-04-28 09:22:20 -0400

    Update mobilescr.js

--- mobilescr.js
+++ mobilescr.js
@@ -23,7 +23,7 @@ window.addEventListener("touchmove", e => {
 
 window.addEventListener("touchend", e => {
   e.preventDefault();
-  console.log(e.touches);
+  console.log(e);
   Array.prototype.forEach.call(new Array(e.touches), tEnd);
 }, {passive: false});
 
