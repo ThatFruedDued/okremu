a002d7bb185c6e0fc4aebc55b9e8eab284bf2dd5
Frued Dued <thatfrueddued@github.com>
2020-04-28 09:24:50 -0400

    Update mobilescr.js

--- mobilescr.js
+++ mobilescr.js
@@ -24,15 +24,15 @@ window.addEventListener("touchmove", e => {
 window.addEventListener("touchend", e => {
   e.preventDefault();
   console.log(e);
-  Array.prototype.forEach.call(new Array(e.touches), tEnd);
+  Array.prototype.forEach.call(new Array(e.changedTouches), tEnd);
 }, {passive: false});
 
 function tEnd(touch){
-  var toRemove = [parseFloat(touch.pageX), parseFloat(touch.pageY)];
-  console.log(touch.pageX);
+  var toRemove = [parseFloat(touch.clientX), parseFloat(touch.clientY)];
+  console.log(touch.clientX);
   var index = touches.indexOf(toRemove);
   touches.splice(index, 1);
-  var removeElem = document.elementFromPoint(parseFloat(touch.pageX), parseFloat(touch.pageY));
+  var removeElem = document.elementFromPoint(parseFloat(touch.clientX), parseFloat(touch.clientY));
   switch(removeElem.id){
     case "bup":
       document.getElementById('frame').contentWindow.pressButtonUp(38);
