ad17f799dcad1f2333850c7a1130856a0b376216
Frued Dued <thatfrueddued@github.com>
2020-06-02 17:29:45 -0400

    Update gameVals.js

--- scripts/gameVals.js
+++ scripts/gameVals.js
@@ -8,7 +8,8 @@
       r.text()
       .then(j => {
         localStorage.setItem("gamevals", j);
-        alert("[CODE OEMU-00001] okrEMU is undergoing an update. You will be reloaded and everything will continue as normal. Please dismiss this popup to use okrEMU.")
+        alert("[CODE OEMU-00001] okrEMU is undergoing an update. You will be reloaded and everything will continue as normal. Please dismiss this popup to use okrEMU.");
+        document.location.reload();
       });
     });
   }
